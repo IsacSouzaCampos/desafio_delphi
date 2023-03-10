90e3c3512e49d629b27db4bdca58ac8beeafe1bb
Isac Campos <isacsouzacampos@hotmail.com>
2023-03-10 11:01:12 -0300

    Inversor de Palavras/Frases.

---
+++ Inverte Palavra/uPrincipal.dfm
@@ -0,0 +1,58 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Inverte Palavra'
+  ClientHeight = 193
+  ClientWidth = 317
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  KeyPreview = True
+  OldCreateOrder = False
+  OnKeyDown = FormKeyDown
+  OnKeyPress = FormKeyPress
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 40
+    Top = 24
+    Width = 75
+    Height = 13
+    Caption = 'Palavra Original'
+  end
+  object Label2: TLabel
+    Left = 40
+    Top = 72
+    Width = 83
+    Height = 13
+    Caption = 'Palavra Invertida'
+  end
+  object Edit1: TEdit
+    Left = 40
+    Top = 43
+    Width = 241
+    Height = 21
+    TabOrder = 0
+    OnExit = Edit1Exit
+  end
+  object Edit2: TEdit
+    Left = 40
+    Top = 91
+    Width = 241
+    Height = 21
+    ReadOnly = True
+    TabOrder = 1
+  end
+  object Button1: TButton
+    Left = 40
+    Top = 153
+    Width = 241
+    Height = 25
+    Caption = 'Inverter'
+    TabOrder = 2
+    OnClick = Button1Click
+  end
+end
---
+++ Inverte Palavra/uPrincipal.pas
@@ -0,0 +1,72 @@
+unit uPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Edit2: TEdit;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormKeyPress(Sender: TObject; var Key: Char);
+    procedure Edit1Exit(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  strInverted: String;
+  intSize    : Integer;
+  I          : Integer;
+begin
+  if Edit1.Text <> '' then
+    begin
+      Edit2.Text := '';
+      intSize := Length(Edit1.Text);
+
+      for I := intSize downto 0 do
+        strInverted := strInverted + Edit1.Text[I];
+
+      Edit2.Text := strInverted;
+    end
+  else
+    ShowMessage('Palavra Original não informada. Digite uma palavra/frase' +
+                  ' a ser invertida!');
+end;
+
+procedure TForm1.Edit1Exit(Sender: TObject);
+begin
+  Button1.SetFocus;
+end;
+
+procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if Key = 13 then
+    Perform(Wm_NextDlgCtl, 0, 0);
+end;
+
+procedure TForm1.FormKeyPress(Sender: TObject; var Key: Char);
+begin
+  if Key = #13 then
+    Key := #0;
+end;
+
+end.
